c03eb1a04a0b312a5eafc5f8e13de2ad393d3085
aownmohammad4772 <mohammadaown1@gmail.com>
2021-05-26 11:23:14 +0500

    Changes in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var text = "my name is Aown Mohammad and my father name is Younas Khan";
 
 for(var i = 0 ; text.length; i++) {
     if (text.slice(i,i + 13) === "Aown Mohammad"){
-        text = text.slice(0, i) + "Waqar Younas";
+        text = text.slice(0, i) + "usama"
     }
 }
 
