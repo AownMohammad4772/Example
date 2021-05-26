96889bb67f5b819f0e0e1ca3f47f5d95e3b70997
aownmohammad4772 <mohammadaown1@gmail.com>
2021-05-26 11:19:02 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var text = "my name is Aown Mohammad and my father name is Younas Khan";
+
+for(var i = 0 ; text.length; i++) {
+    if (text.slice(i,i + 13) === "Aown Mohammad"){
+        text = text.slice(0, i) + "Waqar Younas";
+    }
+}
+
+console.log(text)
